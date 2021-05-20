1e895d7935b9d362f9f14047e5d28acfbeb7e9e4
coutlinx <1402284023@qq.com>
2021-05-20 11:34:31 +0800

    linx

--- Blog/routes/blog_input.js
+++ Blog/routes/blog_input.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('admin/blogs-input');
 });
-
+router.post('/',(req,res)=>{
+  console.log(req.body);
+})
 module.exports = router;
